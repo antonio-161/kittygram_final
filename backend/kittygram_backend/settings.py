f2a35e35f188d53660e885bada7c6d82802d10f9
Anton Stepanov <antonstepanov@yandex.ru>
2025-02-26 18:31:15 +0300

    2222

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['kittygram-161.zapto.org', '62.84.120.29']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
