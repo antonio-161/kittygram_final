d19658f40ae6953afd92159e9fcc166179efe6fc
Anton Stepanov <antonstepanov@yandex.ru>
2025-02-27 12:19:33 +0300

    1111

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(override=True)
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
