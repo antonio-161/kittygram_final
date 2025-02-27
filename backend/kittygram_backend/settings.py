da714a031ed0b374b4d3ed6740446876d8f5bfbe
Anton Stepanov <antonstepanov@yandex.ru>
2025-02-27 17:10:11 +0300

    123

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ load_dotenv(override=True)
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
